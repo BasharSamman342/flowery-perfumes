f50fcf77ae88fc71a79aa8e6340c831933b8a3d1
basharsamman51 <159685891+basharsamman51@users.noreply.github.com>
2025-04-21 04:07:18 +0200

    Feat: top products section [DONE]

---
+++ src/components/TopProductsCard.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useInView } from "react-intersection-observer";
+
+gsap.registerPlugin(useGSAP);
+
+interface Props {
+  inView: boolean;
+  title: string;
+  price: string;
+  image: string;
+  //   bgWidth: string;
+  bgImage: string;
+  description: string;
+  direction: "rtl" | "ltr";
+  marginTop?: string;
+}
+
+export default function TopProductsCard({
+  //   inView,
+  title,
+  price,
+  image,
+  description,
+  direction,
+  bgImage,
+  marginTop = "-200px",
+}: //   bgWidth,
+Props) {
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: `${marginTop} 0px 0px 0px`,
+    // rootMargin: `-200px 0px 0px 0px`,
+    triggerOnce: false,
+  });
+  useEffect(() => {
+    if (inView) {
+      gsap.from("#flowers-log", {
+        rotate: -90,
+        duration: 1,
+        ease: "power2.out",
+      });
+    }
+  }, [inView]);
+  return (
+    <Card ref={ref}>
+      <CardContent>
+        <div
+          className="flex items-center justify-between "
+          style={{ direction: direction }}
+        >
+          <div className="flex flex-col gap-y-5 w-2/5">
+            <h1 className="text-4xl font-semibold kugile">{title}</h1>
+            <p>{description}</p>
+            <h2 className="text-2xl font-semibold bemirs">{price}</h2>
+          </div>
+          <div className="relative w-72 h-72 flex items-center justify-center">
+            <img
+              id="flowers-log"
+              className="absolute z-10"
+              src={bgImage}
+              style={{
+                // width: bgWidth,
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
+            />
+            <img src={image} className="absolute z-20" width={200} alt="" />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
--- src/components/TopProductsSection.tsx
+++ src/components/TopProductsSection.tsx
@@ -1,5 +1,85 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { Card, CardContent } from "@/components/ui/card";
+import TopProductsCard from "./TopProductsCard";
 
 export default function TopProductsSection() {
-  return <div className="min-h-screen">top three</div>;
+  const [animationClass, setAnimationClass] = useState(
+    "tracking-in-contract-bck"
+  );
+  const { ref, inView } = useInView({
+    threshold: 0,
+    rootMargin: "-50px 0px 0px 0px",
+    triggerOnce: false,
+  });
+  useEffect(() => {
+    if (inView) {
+      // setAnimationClass("focus-in-contract-bck");
+      setAnimationClass("tracking-in-contract-bck");
+    } else {
+      setAnimationClass("blur-out-expand-fwd");
+    }
+
+    // console.log("inView", inView);
+
+    // if (inView) {
+    //   gsap.from("#flowers-log", {
+    //     rotate: -90,
+    //     duration: 1,
+    //     ease: "power2.out",
+    //   });
+    // }
+  }, [inView]);
+
+  const topProducts = [
+    {
+      // id:""
+      title: "Rose Goldea",
+      price: "$150",
+      image: "/perfume-bottle-1.png",
+      direction: "ltr" as "ltr" | "rtl",
+      bgImage: "/log-flower.png",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, reiciendis facere doloremque alias soluta, animi optio fugit molestiae similique necessitatibus modi, dignissimos eos! Ipsa dolorem eligendi maxime omnis aperiam explicabo.",
+    },
+    {
+      // id:""
+      title: "Monique Lhuillier",
+      price: "$250",
+      image: "/perfume-bottle-3.png",
+      direction: "rtl" as "ltr" | "rtl",
+      bgImage: "/log-flower-2.png",
+      marginTop: "-300px",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, reiciendis facere doloremque alias soluta, animi optio fugit molestiae similique necessitatibus modi, dignissimos eos! Ipsa dolorem eligendi maxime omnis aperiam explicabo.",
+    },
+    {
+      // id:""
+      title: "Valentino",
+      price: "$350",
+      image: "/perfume-bottle-2.png",
+      direction: "ltr" as "ltr" | "rtl",
+      bgImage: "/log-flower-3.png",
+      marginTop: "-400px",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, reiciendis facere doloremque alias soluta, animi optio fugit molestiae similique necessitatibus modi, dignissimos eos! Ipsa dolorem eligendi maxime omnis aperiam explicabo.",
+    },
+  ];
+
+  return (
+    <div className="min-h-screen">
+      <div className="w-[85%] mx-auto">
+        <div className="flex flex-col gap-y-5">
+          <h1 className="text-4xl text-center kugile border-b-2 border-black tracking-in-expand">
+            Top fragrances of this week
+          </h1>
+          <div className="flex flex-col gap-y-5">
+            {topProducts.map((item) => {
+              return <TopProductsCard {...item} inView={inView} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
